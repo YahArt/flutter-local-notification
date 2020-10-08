8f0f87c6bbc18228aeec56c1f22868577536d3d6
hutter <hutteryannick@gmail.com>
2020-10-08 17:06:09 +0200

    Added basic notification example

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() => runApp(NotificationApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
-class NotificationApp extends StatelessWidget {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+  });
+
+  runApp(NotificationApp());
+}
+
+class NotificationApp extends StatefulWidget {
+  NotificationApp();
+
+  @override
+  _NotificationAppState createState() => _NotificationAppState();
+}
+
+class _NotificationAppState extends State<NotificationApp> {
+  Future<void> _showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+            'your channel id', 'your channel name', 'your channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            showWhen: true);
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'some payload');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeWidget(),
-    );
-  }
-}
-
-class HomeWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Widget'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          child: Text('Home Widget'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Widget'),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await _showNotification();
+          },
+          child: Icon(Icons.time_to_leave),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Home Widget'),
+          ),
         ),
       ),
     );
